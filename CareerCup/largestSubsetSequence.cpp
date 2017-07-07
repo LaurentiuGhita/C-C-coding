e738bc096ed5ba74b0c1cca6f220fb5d0c8b4f88
laur <laur0791@gmail.com>
2017-07-07 23:06:24 +0300

    Added career cup problems

---
+++ CareerCup/largestSubsetSequence.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+/*
+end element of the sequence points to beginning element of the sequence 
+and beginning element of the sequence points to the end element.
+*/
+void subsetSequence(std::vector<int>& array)
+{
+
+	//each element has the minimum lower bound ()
+	std::map<int, int > resSequencesMap;
+	int firstIndex = 0;
+	int endIndex = 0;
+
+	for(int element : array)
+	{
+		// element not in map
+		if(resSequencesMap.find(element) == resSequencesMap.end())
+		{
+			int begining = element;
+			int end = element;
+			resSequencesMap[element] = element;
+
+			if(resSequencesMap.find(element - 1) != resSequencesMap.end())
+				begining = resSequencesMap[element-1];
+
+			if(resSequencesMap.find(element + 1) != resSequencesMap.end())
+				end = resSequencesMap[element + 1];
+
+			int aux = resSequencesMap[begining];
+			resSequencesMap[begining] = resSequencesMap[end];
+			resSequencesMap[end] = aux;
+
+			if(end - begining >= endIndex - firstIndex)
+			{
+				endIndex = end;
+				firstIndex = begining;
+			} 
+		}
+	}
+
+	std::cout << "Longest subsequence from " << firstIndex << " to " << endIndex << "\n";
+
+}
+
+
+
+int main()
+{
+	int nSize;
+	std::cin >> nSize;
+	std::vector<int> array;
+
+	for(int i = 0; i < nSize; ++i)
+	{
+		int aux;
+		std::cin >> aux;
+		array.push_back(aux);
+	}
+
+	subsetSequence(array);
+}
